446c6976f10369561f5a51a649663132ad696a93
Jawad <jawad.taj1998@gmail.com>
2025-12-16 01:38:18 +0600

    test

--- frontend/src/app/api/api.ts
+++ frontend/src/app/api/api.ts
@@ -1,14 +1,5 @@
-import axios from 'axios'
+import api, { API_BASE_URL } from '@/lib/axios'
 import { useAuthStore } from '@/store/useAuthStore'
-
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api'
-
-const api = axios.create({
-    baseURL: API_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
-})
 
 api.interceptors.request.use(
     (config) => {
